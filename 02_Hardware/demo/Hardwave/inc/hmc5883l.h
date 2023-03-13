ccb4764f0620913f40ee76d7b182d7d4d948f3db
TianKuan <tian-kuan@foxmail.com>
2023-03-14 00:00:22 +0800

    更新

--- 02_Hardware/demo/Hardwave/inc/hmc5883l.h
+++ 02_Hardware/demo/Hardwave/inc/hmc5883l.h
@@ -1,20 +1,78 @@
-#ifndef __MCP4561_H
-#define __MCP4561_H
+#ifndef __HMC5883l_H
+#define __HMC5883l_H
  
 /* Includes ------------------------------------------------------------------*/
 #include "iic.h"  
 
 /* Exported macro ------------------------------------------------------------*/
-#define MCP4561_FULL_RANGE	(256u)
+
+/* device address */
+#define HMC5883_ADDR			(0X1E)	//7-bit address			:	0001 1110
+#define HMC5883_WRITE_ADDR		(0X3C)	//8-bit read address	:	0011 1100
+#define HMC5883_READ_ADDR		(0X3D)	//8-bit read address	:	0011 1101
+
+
+/* Register address */
+#define HMC5883_REG_CONFIGA 	(0x00)	//配置寄存器A：用于配置该装置设置的数据输出速率和测量配置
+#define HMC5883_REG_CONFIGB 	(0x01)	//配置寄存器B: 用于设置装置的增益
+#define HMC5883_REG_MODE 		(0x02)	//用来设定装置的操作模式
+#define HMC5883_REG_X_MSB 		(0x03)	//输出X寄存器A中存储测量结果中的MSB（高位数据）
+#define HMC5883_REG_X_LSB 		(0x04)	//输出X寄存器B中存储测量结果中的MSB（低位数据）
+#define HMC5883_REG_Z_MSB 		(0x05)	//输出Y寄存器A中存储测量结果中的MSB（高位数据）
+#define HMC5883_REG_Z_LSB 		(0x06)	//输出Y寄存器B中存储测量结果中的MSB（低位数据）
+#define HMC5883_REG_Y_MSB 		(0x07)	//输出Z寄存器A中存储测量结果中的MSB（高位数据） 
+#define HMC5883_REG_Y_LSB 		(0x08)	//输出Z寄存器B中存储测量结果中的MSB（低位数据） 
+#define HMC5883_REG_IDENTA		(0x09)	//识别寄存器A
+#define HMC5883_REG_IDENTB		(0x0A)	//识别寄存器B
+#define HMC5883_REG_IDENTC		(0x0B)	//识别寄存器C
+
+
+/* Configuration Register A */
+//CRA7:Bit CRA7 is reserved for future function. Set to 0 when configuring CRA.
+//MA1 to MA0(CRA6 to CRA5):Select number of samples averaged (1 to 8) per measurement output.
+#define HMC5883_REG_CRA_MS_NORAL     (0x00)	//default
+#define HMC5883_REG_CRA_MS_XYZ_P     (0x01)
+#define HMC5883_REG_CRA_MS_XYZ_N     (0x02)
+#define HMC5883_REG_CRA_MS_REV       (0x03)
+//MA4 to MA2(CRA4 to CRA2):Data Output Rate Bits. 
+#define HMC5883_REG_CRA_DO_Hz_0_75   (0x00)
+#define HMC5883_REG_CRA_DO_Hz_1_5    (0x04)
+#define HMC5883_REG_CRA_DO_Hz_3      (0x08)
+#define HMC5883_REG_CRA_DO_Hz_7_5    (0x0C)
+#define HMC5883_REG_CRA_DO_Hz_15     (0x10) //default
+#define HMC5883_REG_CRA_DO_Hz_30     (0x14)
+#define HMC5883_REG_CRA_DO_Hz_75     (0x18)
+#define HMC5883_REG_CRA_DO_Hz_NONE   (0x1C)
+//MA1 to MA0(CRA1 to CRA0):Measurement Configuration Bits. 
+#define HMC5883_REG_CRA_MA_AVG_1     (0x00)
+#define HMC5883_REG_CRA_MA_AVG_2     (0x20)
+#define HMC5883_REG_CRA_MA_AVG_4     (0x40)
+#define HMC5883_REG_CRA_MA_AVG_8     (0x60) //default
+	
+
+/* Configuration Register B */
+//GN2 to GN0(CRB7 to CRB5):Gain Configuration Bits. PS:(CRB4 to CRB0)These bits must be cleared for correct operation.
+#define HMC5883_REG_GN_G1370    (0x00)
+#define HMC5883_REG_GN_G1090    (0x20)	// default
+#define HMC5883_REG_GN_G820     (0x40)
+#define HMC5883_REG_GN_G660     (0x60)
+#define HMC5883_REG_GN_G440     (0x80)
+#define HMC5883_REG_GN_G390     (0xA0)
+#define HMC5883_REG_GN_G330     (0xC0)
+#define HMC5883_REG_GN_G230     (0xE0)
+
+
+/* Mode Register */   
+//MD1 to MD0(MR1 to MR0):Mode Select Bits.PS:(MR7 to MR2)Set this pin to enable High Speed I2C, 3400kHz.
+#define HMC5883_REG_MODE_MR_CON      (0x00)    
+#define HMC5883_REG_MODE_MR_SIG      (0x01) // default   
+#define HMC5883_REG_MODE_MR_IDLE1    (0x02)    
+#define HMC5883_REG_MODE_MR_IDLE2    (0x03) 
+
 
 /* Exported functions ------------------------------------------------------- */
-void MCP4561_Init(void);
-void MCP4561_Test(void);
-void MCP4561_ManualAdjust(void);
-
-ErrStatus MCP4561_SetResistor(uint16_t val);
-ErrStatus MCP4561_SaveResistor(uint16_t val);
-uint16_t MCP4561_GetResistor(void);
+void HMC5883lInit(void);
+void HMC5883lRead(int16_t *mag_x, int16_t *mag_y, int16_t *mag_z);
 
 #endif
 
--- 02_Hardware/demo/Hardwave/src/hmc5883l.c
+++ 02_Hardware/demo/Hardwave/src/hmc5883l.c
@@ -34,69 +34,66 @@
 
 /** ---	Start of Macro define 	---------------------------------------------------------------------  **/
 
-/* device address */
-#define HMC5883_ADDR			(0X1E)	//7-bit address			:	0001 1110
-#define HMC5883_WRITE_ADDR		(0X3C)	//8-bit read address	:	0011 1100
-#define HMC5883_READ_ADDR		(0X3D)	//8-bit read address	:	0011 1101
+/** ---	Start of Para define 	---------------------------------------------------------------------  **/
 
+/** ---	Start of Funtion define ---------------------------------------------------------------------  **/
 
-/* Register address */
-#define HMC5883_REG_CONFIGA 	(0x00)	//配置寄存器A：用于配置该装置设置的数据输出速率和测量配置
-#define HMC5883_REG_CONFIGB 	(0x01)	//配置寄存器B: 用于设置装置的增益
-#define HMC5883_REG_MODE 		(0x02)	//用来设定装置的操作模式
-#define HMC5883_REG_X_MSB 		(0x03)	//输出X寄存器A中存储测量结果中的MSB（高位数据）
-#define HMC5883_REG_X_LSB 		(0x04)	//输出X寄存器B中存储测量结果中的MSB（低位数据）
-#define HMC5883_REG_Z_MSB 		(0x05)	//输出Y寄存器A中存储测量结果中的MSB（高位数据）
-#define HMC5883_REG_Z_LSB 		(0x06)	//输出Y寄存器B中存储测量结果中的MSB（低位数据）
-#define HMC5883_REG_Y_MSB 		(0x07)	//输出Z寄存器A中存储测量结果中的MSB（高位数据） 
-#define HMC5883_REG_Y_LSB 		(0x08)	//输出Z寄存器B中存储测量结果中的MSB（低位数据） 
-#define HMC5883_REG_IDENTA		(0x09)	//识别寄存器A
-#define HMC5883_REG_IDENTB		(0x0A)	//识别寄存器B
-#define HMC5883_REG_IDENTC		(0x0B)	//识别寄存器C
+/** ---	芯片操作函数 -----------------------------------  **/
 
+static int i2cWrite(uint8_t reg_, uint8_t Data)
+{
+	return i2c_mem_write(MAG_ADDRESS, reg_, &Data, 1);
+}
 
-/* Configuration Register A */
-//CRA7:Bit CRA7 is reserved for future function. Set to 0 when configuring CRA.
-//MA1 to MA0(CRA6 to CRA5):Select number of samples averaged (1 to 8) per measurement output.
-#define HMC5883_REG_CRA_MS_NORAL     (0x00)	//default
-#define HMC5883_REG_CRA_MS_XYZ_P     (0x01)
-#define HMC5883_REG_CRA_MS_XYZ_N     (0x02)
-#define HMC5883_REG_CRA_MS_REV       (0x03)
-//MA4 to MA2(CRA4 to CRA2):Data Output Rate Bits. 
-#define HMC5883_REG_CRA_DO_Hz_0_75   (0x00)
-#define HMC5883_REG_CRA_DO_Hz_1_5    (0x04)
-#define HMC5883_REG_CRA_DO_Hz_3      (0x08)
-#define HMC5883_REG_CRA_DO_Hz_7_5    (0x0C)
-#define HMC5883_REG_CRA_DO_Hz_15     (0x10) //default
-#define HMC5883_REG_CRA_DO_Hz_30     (0x14)
-#define HMC5883_REG_CRA_DO_Hz_75     (0x18)
-#define HMC5883_REG_CRA_DO_Hz_NONE   (0x1C)
-//MA1 to MA0(CRA1 to CRA0):Measurement Configuration Bits. 
-#define HMC5883_REG_CRA_MA_AVG_1     (0x00)
-#define HMC5883_REG_CRA_MA_AVG_2     (0x20)
-#define HMC5883_REG_CRA_MA_AVG_4     (0x40)
-#define HMC5883_REG_CRA_MA_AVG_8     (0x60) //default
-	
+static int i2cRead(uint8_t reg_, uint8_t len, uint8_t* buf)
+{
+	return i2c_mem_read(MAG_ADDRESS, reg_, buf, len);
+}
 
-/* Configuration Register B */
-//GN2 to GN0(CRB7 to CRB5):Gain Configuration Bits. PS:(CRB4 to CRB0)These bits must be cleared for correct operation.
-#define HMC5883_REG_GN_G1370    (0x00)
-#define HMC5883_REG_GN_G1090    (0x20)	// default
-#define HMC5883_REG_GN_G820     (0x40)
-#define HMC5883_REG_GN_G660     (0x60)
-#define HMC5883_REG_GN_G440     (0x80)
-#define HMC5883_REG_GN_G390     (0xA0)
-#define HMC5883_REG_GN_G330     (0xC0)
-#define HMC5883_REG_GN_G230     (0xE0)
+/**
+* @brief	xmc5883初始化,包含传感器校准
+* @note
+*/
+void xmc5883lInit(void)
+{
+#ifdef HMC5883L
+	i2cWrite(ConfigRegA, Sample_MAX << 5 | DataOutputRate_Max << 2 | NormalOperation);
+	i2cWrite(ConfigRegB, Full_Scale_1_9G << 5);
+	i2cWrite(ModeRegister, ContinuousConversion);	
+#endif
+#ifdef QMC5883L
+	i2cWrite(ConfigReg1, Sample_MAX << 6 | Full_Scale_2G << 4 | DataOutputRate_Max << 2 | ContinuousConversion);
+	i2cWrite(ConfigReg2, Enable_Interrupt_PIN << 7 | ROL_PNT_Normal << 6 | SOFT_RST_Normal);
+	i2cWrite(Period_FBR, 0x01);
+#endif
+}
 
-
-/* Mode Register */   
-//MD1 to MD0(MR1 to MR0):Mode Select Bits.PS:(MR7 to MR2)Set this pin to enable High Speed I2C, 3400kHz.
-#define HMC5883_REG_MODE_MR_CON      (0x00)    
-#define HMC5883_REG_MODE_MR_SIG      (0x01) // default   
-#define HMC5883_REG_MODE_MR_IDLE1    (0x02)    
-#define HMC5883_REG_MODE_MR_IDLE2    (0x03) 
-
+/**
+* @brief	读取磁场传感器数据。
+* @note
+* @param	magData：存储磁场传感器原始数据的指针
+*/
+#ifdef QMC5883L
+	int16_t QMC5883L_Temp = 0;
+#endif
+void xmc5883lRead(int16_t *mag_x, int16_t *mag_y, int16_t *mag_z)
+{
+#ifdef HMC5883L
+	uint8_t buf[6];
+	i2cRead(MAG_DATA_REGISTER, 6, buf);
+	*mag_x = buf[0] << 8 | buf[1];
+	*mag_z = buf[2] << 8 | buf[3];
+	*mag_y = buf[4] << 8 | buf[5];
+#endif
+#ifdef QMC5883L
+	uint8_t buf[9];
+	i2cRead(MAG_DATA_REGISTER, 9, buf);
+	*mag_x = buf[0] | buf[1] << 8;
+	*mag_y = buf[2] | buf[3] << 8;
+	*mag_z = buf[4] | buf[5] << 8;
+	QMC5883L_Temp = buf[7] | buf[8] << 8;
+#endif
+}
 
 
 
